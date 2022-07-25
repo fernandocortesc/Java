799a17fa52699217d3d1973bfbe0218cd431467b
fernandocortesc <fernando@cortescalderon.com>
2022-07-24 21:39:00 -0500

    Eliminación de carpetas innecesarias

---
+++ Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+    public static void main(String args[]){
+        //Variable de tipo entero
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+    }
+}
